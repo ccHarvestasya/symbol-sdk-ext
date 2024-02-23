ae1ba8f4ae7bd4ad70f9f3c73bd35f3174260a80
ccHarvestasya <harvestasya84@gmail.com>
2024-02-23 20:50:26 +0900

    isEnableHttps修正

--- src/SymbolSdkExt.ts
+++ src/SymbolSdkExt.ts
@@ -14,25 +14,21 @@ export class SymbolSdkExt {
    */
   isEnableHttps(host: string): Promise<boolean> {
     return new Promise((resolve) => {
-      try {
-        const socket = TLS.connect(
-          { host: host, port: 3001, timeout: this.timeout, servername: host },
-          () => {
-            resolve(true);
-            socket.destroy();
-          }
-        );
-        socket.on('timeout', () => {
-          resolve(false);
+      const socket = TLS.connect(
+        { host: host, port: 3001, timeout: this.timeout, servername: host },
+        () => {
+          resolve(true);
           socket.destroy();
-        });
-        socket.on('error', () => {
-          resolve(false);
-          socket.destroy();
-        });
-      } catch (e) {
+        }
+      );
+      socket.on('timeout', () => {
         resolve(false);
-      }
+        socket.destroy();
+      });
+      socket.on('error', () => {
+        resolve(false);
+        socket.destroy();
+      });
     });
   }
 }
